0219e0531ae0ea99f6f62b371a66f8ecba80cd24
quocle <quoc.le@futa.vn>
2025-03-05 10:55:29 +0700

    handle echo endpoint

--- go.mod
+++ go.mod
@@ -5,8 +5,16 @@ go 1.21.5
 require github.com/stretchr/testify v1.10.0
 
 require (
+	go.uber.org/dig v1.18.0 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/sys v0.0.0-20220412211240-33da011f77ad // indirect
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
+	go.uber.org/fx v1.23.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,10 +1,66 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"testify-examples/morestrings"
+	"io"
+	"net"
+	"net/http"
+	"os"
+
+	"go.uber.org/fx"
 )
 
 func main() {
-	fmt.Println(morestrings.ReverseRunes(".dlrow ,olleH"))
+	fx.New(
+		fx.Provide(
+			NewHTTPServer,
+			NewEchoHandler,
+			NewServeMux,
+		),
+		fx.Invoke(func(server *http.Server) {}),
+	).Run()
+}
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("Starting HTTP server at %s", srv.Addr)
+			go srv.Serve(ln)
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
+
+// EchoHandler is an http.Handler that copies its request body
+// back to the response.
+type EchoHandler struct{}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (*EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+	}
+}
+
+// NewServeMux builds a ServerMux that will route requests
+// to the given EchoHandler.
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
 }
